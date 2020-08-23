5f971e42bae97796b86b92f0ea893985029838bb
Hugh Wang <18501667737@163.com>
2020-08-23 17:55:32 +0800

    a

---
+++ spring-security/src/main/java/com/uwjx/springsecurity/security/MyAccessDeniedHandler.java
@@ -0,0 +1,28 @@
+package com.uwjx.springsecurity.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Slf4j
+@Component
+public class MyAccessDeniedHandler implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request,
+                       HttpServletResponse response,
+                       AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        response.setContentType("application/json;charset=UTF-8");
+        PrintWriter printWriter = response.getWriter();
+        printWriter.write("无权限访问 , 请联系管理员 MyAccessDeniedHandler");
+        printWriter.flush();
+        printWriter.close();
+    }
+}
